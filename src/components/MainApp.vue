63f4e90962ea2d875cad791ddccf60a15439caf9
dumy1994 <dumitru_budau@yahoo.it>
2022-05-16 19:04:16 +0200

    acìrticol post started

--- src/components/MainApp.vue
+++ src/components/MainApp.vue
@@ -3,18 +3,20 @@
    <feautured-post/>
    <app-post/>
    <short-cut/>
+   <articol-post/>
    
   </div>
 </template>
 
 <script>
 import AppPost from './main/AppPost.vue'
+import ArticolPost from './main/ArticolPost.vue'
 import FeauturedPost from './main/FeauturedPost.vue'
 import ShortCut from './main/ShortCut.vue'
 
 
 export default {
-  components: {FeauturedPost, AppPost, ShortCut},
+  components: {FeauturedPost, AppPost, ShortCut, ArticolPost},
  
      name:'MainApp',
   data(){
---
+++ src/components/main/ArticolPost.vue
@@ -0,0 +1,75 @@
+<template>
+    <div class="container-fluid p-0 row">
+        <div class="container-card p-0 col-9 debug">
+          <!-- 1 artical  -->
+            <div v-for="(detail, index) in dati" :key="index">
+              <img src="../../assets/img/blog-54.jpg" alt="">
+                <div class="articol debug2 d-flex">
+                    <div class="date debug col-1">
+                        <div>{{detail.date}}</div>
+                        <div>JAN</div>
+                    </div>
+                    <div class="col-11 debug text">
+                    {{detail.text}}
+                        <div class="">
+                            <span class="name">
+                                {{detail.author}}
+                            </span>
+                            <span class="section">
+                                {{detail.section}}
+                            </span>
+                            <span class="comments">
+                                {{detail.comments}}
+                            </span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
+        </div>
+
+
+      <!-- social -->
+        <div class="social debug2 col-3">
+
+        </div>
+    </div>
+</template>
+
+<script>
+import LastPost from '../../LastPost'
+export default {
+    name: 'ArticolPost',
+    data(){
+        return{
+            dati: LastPost,
+
+        }
+    }
+}
+</script>
+
+
+<style lang="scss" scoped>
+@import "../style/general.scss";
+.container-fluid{
+    width: 80%;
+    margin: 0 auto;
+    .container-card{
+        min-height: 60vh;
+        img{
+            width: 100%;
+        }
+        .articol{
+            width: 100%;
+        }
+    }
+    .social{
+        
+        min-height: 50vh;
+    }
+}
+
+
+
+</style>
