27b5f7b3e948ac11342ecec8b22ca16f71690f97
dumy1994 <dumitru_budau@yahoo.it>
2022-05-18 16:44:28 +0200

    shortcut move right and left, still some problem

--- src/components/main/ShortCut.vue
+++ src/components/main/ShortCut.vue
@@ -1,11 +1,11 @@
 <template>
   <section class="container-fluid p-0 short">
       <ul class="d-flex justify-content-between p-0 align-items-center">
-          <li @click="prevSlide"><i class="fa-solid fa-angle-left"></i></li>
-          <li  v-for="(item,activeIndex) of short" :key="activeIndex">
+          <li @click="prevSlide"><i class="fa-solid text-center  fa-angle-left"></i></li>
+          <li  v-for="(item, activeIndex) in short.slice(n, activeIndex)" :key="activeIndex">
               <a  href="#" >{{item}}</a>
           </li>
-          <li @click="nextSlide"><i  class="fa-solid fa-angle-right"></i></li>
+          <li @click="nextSlide"><i  class="fa-solid text-center fa-angle-right"></i></li>
       </ul>
 
   </section>
@@ -17,25 +17,28 @@ export default {
     data(){
         return{
             short:['GADGETS','PHOTOGRAPHY', 'LIFESTYLE', 'FASHION', 'RECIPES', 'TRAVEL','ARCHITECTURE','REVIEWS','SPORTS','VIDEOS','TECHNOLOGY','DESIGN'],
-            activeIndex: 0,
-            shortArray:'',
+            activeIndex: 5,
+            n: 0,
+            
 
         }
     },
     methods:{
         
         nextSlide(){
-            this.activeIndex = (this.activeIndex === 8) ? 0 : this.activeIndex += 1;
+            this.activeIndex = (this.activeIndex === 13) ? 5 : this.activeIndex += 1;
+            this.n = (this.n <= 5) ? this.n +1 : this.n -= 1; 
             console.log(this.activeIndex)
+            console.log(this.n)
         },
         prevSlide(){
-            this.activeIndex = (this.activeIndex === 0) ? 8 : this.activeIndex -= 1;
+            this.activeIndex = (this.activeIndex <= 5) ? 5 :this.activeIndex -= 1;
+            this.n = (this.n >= 0) ? this.n -=1 : this.n += 1
             console.log(this.activeIndex)
+            console.log(this.n)
         },
     },
-    computed:{
-        subArray(){ return this.array.slice(0,3) }
-    }
+    
 }
 
 </script>
@@ -52,6 +55,7 @@ export default {
         list-style: none;
         li{
             line-height: 10vh;
+            
             a{
                 text-decoration: none;
                 height: 20px;
@@ -65,6 +69,16 @@ export default {
             }
             i{
                 color: $bg-grey-dark2;
+                cursor: pointer;
+                border: 2px solid $bg-grey-dark2;
+                padding: 5px;
+                height: 30px;
+                width: 30px;
+                border-radius: 50%;
+                &:hover{
+                    transform: scale(130%);
+                    
+                }
             }
         }
     }
